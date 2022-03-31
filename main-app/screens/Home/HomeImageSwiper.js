e3a5cacc3cf7db9f37dc003e9e2907f41a864c9e
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-03-30 21:23:56 -0700

    Update Home screen images

--- main-app/screens/Home/HomeImageSwiper.js
+++ main-app/screens/Home/HomeImageSwiper.js
@@ -1,26 +1,26 @@
 import * as React from "react";
 import { Image, StyleSheet } from "react-native";
 import Swiper from "react-native-swiper";
-import Galapagos_homepage_whales from "../../app/assets/images/homescreen/Galapagos_homepage_whales.jpg";
+import Galapagos_homepage_whales from "../../app/assets/images/homescreen/Galapagos_homepage_whales.png";
+import Galapagos_homepage_coral from "../../app/assets/images/homescreen/Galapagos_homepage_coral.png";
+import Galapagos_homepage_reptile from "../../app/assets/images/homescreen/Galapagos_homepage_reptile.png";
+import Galapagos_homepage_tortoise from "../../app/assets/images/homescreen/Galapagos_homepage_tortoise.png";
 
 const images = [
   {
     id: 1,
     source: Galapagos_homepage_whales
   },
-  { id: 2, source: Galapagos_homepage_whales },
-  { id: 3, source: Galapagos_homepage_whales }
+  { id: 2, source: Galapagos_homepage_coral },
+  { id: 3, source: Galapagos_homepage_reptile },
+  { id: 4, source: Galapagos_homepage_tortoise }
 ];
 
 const HomeImageSwiper = () => {
   return (
     <Swiper>
       {images.map((item) => (
-        <Image
-          key={item.id}
-          source={Galapagos_homepage_whales}
-          style={styles.image}
-        />
+        <Image key={item.id} source={item.source} style={styles.image} />
       ))}
     </Swiper>
   );
