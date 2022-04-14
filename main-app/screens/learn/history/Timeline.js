c6ca53e45d1f00880d19613f4a3750ca2f109042
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-14 14:51:10 -0700

    delete unused import in Timeline screen

--- main-app/screens/learn/history/Timeline.js
+++ main-app/screens/learn/history/Timeline.js
@@ -1,13 +1,6 @@
 import "react-native-gesture-handler";
 import * as React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Dimensions,
-  ScrollView
-} from "react-native";
+import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
 import LearnHeader from "../LearnHeader";
 
 class Timeline extends React.Component {
