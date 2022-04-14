a6fb65d0700417d76733f45846ed274d53d14e8f
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-14 14:34:20 -0700

    Create LearnHeader Component

---
+++ main-app/screens/learn/LearnHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Header } from "../../components";
+
+const LearnHeader = (props) => {
+  return <Header {...props} title="Learn" color="#FFB800" />;
+};
+
+export default LearnHeader;
