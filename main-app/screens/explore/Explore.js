7292996d882a9e4ce6a6c7919922b26aaab48e59
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-15 12:50:14 -0700

    Refact Explore screen

--- main-app/screens/explore/Explore.js
+++ main-app/screens/explore/Explore.js
@@ -1,110 +1,80 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import {StyleSheet, Button, Text, View, TouchableOpacity, Image, Dimensions,} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-
-//get scaling factors
-const entireScreenWidth = Dimensions.get('window').width;
-let rem;
-rem = entireScreenWidth / 350;
-
-const styles = StyleSheet.create ({
-    header: {
-        fontSize: 34*rem,
-        fontWeight: "600",
-        paddingVertical: 16,
-        paddingLeft: 34,
-        paddingRight: 59
-    },
-    buttonText:{
-        fontSize: 17*rem,
-        paddingLeft: 16
-    },
-    buttonContainer:{
-        flexDirection: 'row',
-        borderBottomWidth: 1,
-        borderColor: '#C0C0C0',
-        paddingVertical: 10,
-        paddingLeft: 34,
-        paddingRight: 14
-    },
-    lastButtonContainer:{
-        flexDirection: 'row',
-        borderBottomWidth: 1,
-        paddingVertical: 10,
-        borderColor: '#C0C0C0',
-        paddingLeft: 34,
-        paddingRight: 14
-    },
-    buttonLeft:{
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    buttonRight:{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    chevron:{
-        width: 11*rem,
-        height: 18*rem
-    }
-});
-
+import "react-native-gesture-handler";
+import * as React from "react";
+import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { NavigationButton, Icon } from "../../components";
+import ExploreHeader from "./ExploreHeader";
 
 class Explore extends React.Component {
-    render(){
-        return (
-            <View style={{backgroundColor: 'white', flex: 1}}>
-                <Image
-                    source={require('../../app/assets/images/Explore.png')}
-                    style={{width: entireScreenWidth, height: 200}}
-                />
+  constructor() {
+    super();
+    this.buttons = [
+      {
+        id: 1,
+        name: "Top Activity",
+        navigatePath: "TopActivities",
+        icon: <Icon name="top-activities" size={26} color={styles.icon.color} />
+      },
+      {
+        id: 2,
+        name: "Travel Agencies",
+        navigatePath: "TravelAgencies",
+        icon: <Icon name="agencies" size={28} color={styles.icon.color} />
+      },
+      {
+        id: 3,
+        name: "Hotels",
+        navigatePath: "SantaCruzHotels",
+        icon: <Icon name="hotels" size={28} color={styles.icon.color} />
+      },
+      {
+        id: 4,
+        name: "Food & Drinks",
+        navigatePath: "SantaCruzFood",
+        icon: (
+          <Icon name="food-and-drinks" size={28} color={styles.icon.color} />
+        )
+      },
+      {
+        id: 5,
+        name: "Want to Island Hop",
+        navigatePath: "IslandHop",
+        icon: <Icon name="island-hop" size={24} color={styles.icon.color} />
+      }
+    ];
+  }
 
-                <TouchableOpacity
-                    style={styles.buttonContainer}
-                    onPress={() => this.props.navigation.navigate("KnowBefore")}>
-                    <View style={styles.buttonLeft}>
-                        <Image
-                            source={require('../../app/assets/icons/knowBefore_gray.png')}
-                            style={{width: 28*rem, height: 28*rem}}
-                        />
-                        <Text style={styles.buttonText}>Know Before You Go</Text>
-                    </View>
-                    <View style={styles.buttonRight}>
-                        <Image
-                            source={require('../../app/assets/icons/chevron.png')}
-                            style={styles.chevron}
-                        />
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.lastButtonContainer}
-                    onPress={() => this.props.navigation.navigate("Plan")}>
-                    <View style={styles.buttonLeft}>
-                        <Image
-                            source={require('../../app/assets/icons/plan_gray.png')}
-                            style={{width: 27*rem, height: 30*rem}}
-                        />
-                        <Text style={styles.buttonText}>Plan Your Trip</Text>
-                    </View>
-                    <View style={styles.buttonRight}>
-                        <Image
-                            source={require('../../app/assets/icons/chevron.png')}
-                            style={styles.chevron}
-                        />
-                    </View>
-                    <View style={{borderBottomWidth: 1}}></View>
-                </TouchableOpacity>
-                <Image
-                    source={require('../../app/assets/images/g2.webp')}
-                    style={{width: entireScreenWidth, height: 355 * rem}}
-                />
-            </View>
-        );
-    }
+  navigate(pageName) {
+    // eslint-disable-next-line react/prop-types
+    this.props.navigation.navigate(pageName);
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <ExploreHeader />
+          {this.buttons.map(({ id, name, navigatePath, icon }) => (
+            <NavigationButton
+              key={id}
+              label={name}
+              onPress={() => this.navigate(navigatePath)}
+              icon={icon}
+            />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default Explore;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFFFFF",
+    flex: 1
+  },
+  icon: {
+    color: "#8B8B8B"
+  },
+  scrollView: { backgroundColor: "#FFFFFF" }
+});
---
+++ main-app/screens/explore/ExploreHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { Header } from "../../components";
+const ExploreHeader = (props) => {
+  return <Header {...props} title="EXPLORE" color="#294B95" icon={"explore"} />;
+};
+
+export default ExploreHeader;
