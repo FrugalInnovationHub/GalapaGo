b76cb90fe529f4921d7bd73bc0673e695426a286
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-20 14:03:31 -0700

    update icon

--- main-app/screens/explore/ExploreHeader.jsx
+++ main-app/screens/explore/ExploreHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Header } from "../../components";
 const ExploreHeader = (props) => {
-  return <Header {...props} title="EXPLORE" color="#294B95" icon={"explore"} />;
+  return <Header {...props} title="EXPLORE" color="#294B95" icon="explore_1" />;
 };
 
 export default ExploreHeader;
