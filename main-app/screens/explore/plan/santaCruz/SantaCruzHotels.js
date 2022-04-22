e7382c7a55cd1c01e4832337a72509e34fe4a1a7
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-22 14:46:47 -0700

    update hotel screen css

--- main-app/screens/explore/plan/santaCruz/SantaCruzHotels.js
+++ main-app/screens/explore/plan/santaCruz/SantaCruzHotels.js
@@ -17,140 +17,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import OpenMap from "react-native-open-map";
 import * as Items from "../../../../app/assets/images/hotels1";
 import ExploreHeader from "../../ExploreHeader";
-//get scaling factors
-const entireScreenWidth = Dimensions.get("window").width;
-let rem;
-rem = entireScreenWidth / 350;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    flex: 1
-  },
-  header: {
-    flexDirection: "row",
-    paddingTop: 16,
-    paddingBottom: 16,
-    paddingLeft: 34,
-    alignItems: "center"
-  },
-  headerText: {
-    paddingLeft: 16,
-    fontSize: 28 * rem,
-    fontWeight: "600"
-  },
-  buttonText: {
-    fontSize: 17 * rem,
-    paddingLeft: 16
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    borderTopWidth: 1,
-    borderColor: "#C0C0C0",
-    paddingVertical: 10,
-    paddingLeft: 34,
-    paddingRight: 14
-  },
-  lastButtonContainer: {
-    flexDirection: "row",
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-    paddingVertical: 10,
-    borderColor: "#C0C0C0",
-    paddingLeft: 34,
-    paddingRight: 14
-  },
-  buttonLeft: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  buttonRight: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center"
-  },
-  chevron: {
-    width: 11 * rem,
-    height: 18 * rem
-  },
-  regularBold: {
-    fontWeight: "600",
-    color: "#000000",
-    fontSize: 17 * rem,
-    paddingLeft: 30,
-    paddingTop: 16,
-    paddingBottom: 16,
-    flexWrap: "wrap"
-  },
-  regular: {
-    fontSize: 17 * rem,
-    color: "#616161",
-    padding: 30,
-    lineHeight: 20 * rem
-  },
-  numberRow: {
-    flexDirection: "row",
-    paddingTop: 16,
-    paddingBottom: 16,
-    paddingLeft: 34,
-    alignItems: "stretch"
-  },
-  wrapper: {
-    height: 200 * rem
-  },
-  slide: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: Dimensions.get("window").width
-  },
-  slideImage: {
-    height: 250 * rem,
-    width: Dimensions.get("window").width
-  },
-  text: {
-    color: "#fff",
-    fontSize: 30,
-    fontWeight: "bold"
-  },
-  infoPhone: {
-    width: 20 * rem,
-    height: 18 * rem
-  },
-  infoEmail: {
-    width: 21 * rem,
-    height: 16 * rem
-  },
-  infoAddress: {
-    width: 18 * rem,
-    height: 21 * rem
-  },
-  infoWeb: {
-    width: 20 * rem,
-    height: 20 * rem
-  },
-  infoText: {
-    fontSize: 17 * rem,
-    color: "#616161",
-    lineHeight: 20 * rem,
-    marginLeft: 10 * rem,
-    marginRight: 20 * rem
-  },
-  numberRow: {
-    flexDirection: "row",
-    paddingTop: 20,
-    justifyContent: "center",
-    flexWrap: "wrap"
-  },
-  lastRow: {
-    flexDirection: "row",
-    paddingTop: 20,
-    paddingBottom: 50,
-    justifyContent: "center",
-    flexWrap: "wrap"
-  }
-});
 
 const data = require("../../../../data/Hotels.json");
 
@@ -200,7 +66,6 @@ class SantaCruzHotels extends React.Component {
           style={{width: entireScreenWidth, height: 25*rem}}
           />*/}
           {this.state.hotels.map((hotel, index) => {
-            const temp = Items.coffeelab1;
             return (
               <View key={index}>
                 <Text style={styles.regularBold}>{hotel.name}</Text>
@@ -465,3 +330,96 @@ class SantaCruzHotels extends React.Component {
 }
 
 export default SantaCruzHotels;
+
+//get scaling factors
+const entireScreenWidth = Dimensions.get("window").width;
+let rem;
+rem = entireScreenWidth / 350;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1
+  },
+  header: {
+    flexDirection: "row",
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 34,
+    alignItems: "center"
+  },
+  headerText: {
+    paddingLeft: 16,
+    fontSize: 28 * rem,
+    fontWeight: "600"
+  },
+  regularBold: {
+    fontWeight: "600",
+    color: "#000000",
+    fontSize: 17 * rem,
+    paddingLeft: 30,
+    paddingTop: 16,
+    paddingBottom: 16,
+    flexWrap: "wrap"
+  },
+  numberRow: {
+    flexDirection: "row",
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 34,
+    alignItems: "stretch"
+  },
+  wrapper: {
+    height: 200 * rem
+  },
+  slide: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: Dimensions.get("window").width
+  },
+  slideImage: {
+    height: 250 * rem,
+    width: Dimensions.get("window").width
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  infoPhone: {
+    width: 20 * rem,
+    height: 18 * rem
+  },
+  infoEmail: {
+    width: 21 * rem,
+    height: 16 * rem
+  },
+  infoAddress: {
+    width: 18 * rem,
+    height: 21 * rem
+  },
+  infoWeb: {
+    width: 20 * rem,
+    height: 20 * rem
+  },
+  infoText: {
+    fontSize: 17 * rem,
+    color: "#616161",
+    lineHeight: 20 * rem,
+    marginLeft: 10 * rem,
+    marginRight: 20 * rem
+  },
+  numberRow: {
+    flexDirection: "row",
+    paddingTop: 20,
+    justifyContent: "center",
+    flexWrap: "wrap"
+  },
+  lastRow: {
+    flexDirection: "row",
+    paddingTop: 20,
+    paddingBottom: 50,
+    justifyContent: "center",
+    flexWrap: "wrap"
+  }
+});
