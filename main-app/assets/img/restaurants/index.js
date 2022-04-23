2700012c653092a3b91772e2e66a5464ddedb793
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-22 22:36:56 -0700

    move images

---
+++ main-app/assets/img/restaurants/index.js
@@ -0,0 +1,108 @@
+const restaurantImages = {
+  aquelarre: {
+    images: [
+      { title: "1", file: require("./aquelarre/aquelarre1.jpg") },
+      { title: "2", file: require("./aquelarre/aquelarre2.jpg") },
+      { title: "3", file: require("./aquelarre/aquelarre3.jpg") },
+      { title: "4", file: require("./aquelarre/aquelarre4.jpg") }
+    ]
+  },
+  bahia: {
+    images: [
+      { title: "1", file: require("./bahia/bahia1.jpg") },
+      { title: "2", file: require("./bahia/bahia2.jpg") },
+      { title: "3", file: require("./bahia/bahia3.jpg") },
+      { title: "4", file: require("./bahia/bahia4.jpg") }
+    ]
+  },
+  barbongo: {
+    images: [
+      { title: "1", file: require("./barbongo/barbongo1.jpg") },
+      { title: "2", file: require("./barbongo/barbongo2.jpg") },
+      { title: "3", file: require("./barbongo/barbongo3.jpg") },
+      { title: "4", file: require("./barbongo/barbongo4.jpg") }
+    ]
+  },
+  cevicheria: {
+    images: [
+      { title: "1", file: require("./cevicheria/cevicheria1.jpg") },
+      { title: "2", file: require("./cevicheria/cevicheria2.jpg") },
+      { title: "3", file: require("./cevicheria/cevicheria3.jpg") },
+      { title: "4", file: require("./cevicheria/cevicheria4.jpg") }
+    ]
+  },
+  coffeelab: {
+    images: [
+      { title: "1", file: require("./coffeelab/coffeelab1.jpg") },
+      { title: "2", file: require("./coffeelab/coffeelab2.jpg") },
+      { title: "3", file: require("./coffeelab/coffeelab3.jpg") },
+      { title: "4", file: require("./coffeelab/coffeelab4.jpg") }
+    ]
+  },
+  discotecalapanga: {
+    images: [
+      { title: "1", file: require("./discotecalapanga/discotecalapanga1.jpg") },
+      { title: "2", file: require("./discotecalapanga/discotecalapanga2.jpg") },
+      { title: "3", file: require("./discotecalapanga/discotecalapanga3.jpg") },
+      { title: "4", file: require("./discotecalapanga/discotecalapanga4.jpg") }
+    ]
+  },
+  elmuellededarwin: {
+    images: [
+      { title: "1", file: require("./elmuellededarwin/elmuellededarwin1.jpg") },
+      { title: "2", file: require("./elmuellededarwin/elmuellededarwin2.jpg") },
+      { title: "3", file: require("./elmuellededarwin/elmuellededarwin3.jpg") },
+      { title: "4", file: require("./elmuellededarwin/elmuellededarwin4.jpg") }
+    ]
+  },
+  garrapata: {
+    images: [
+      { title: "1", file: require("./garrapata/garrapata1.jpg") },
+      { title: "2", file: require("./garrapata/garrapata2.jpg") },
+      { title: "3", file: require("./garrapata/garrapata3.jpg") },
+      { title: "4", file: require("./garrapata/garrapata4.jpg") }
+    ]
+  },
+  ilgiardino: {
+    images: [
+      { title: "1", file: require("./ilgiardino/ilgiardino1.jpg") },
+      { title: "2", file: require("./ilgiardino/ilgiardino2.jpg") },
+      { title: "3", file: require("./ilgiardino/ilgiardino3.jpg") },
+      { title: "4", file: require("./ilgiardino/ilgiardino4.jpg") }
+    ]
+  },
+  santacruzbrewery: {
+    images: [
+      { title: "1", file: require("./santacruzbrewery/santacruzbrewery1.jpg") },
+      { title: "2", file: require("./santacruzbrewery/santacruzbrewery2.jpg") },
+      { title: "3", file: require("./santacruzbrewery/santacruzbrewery3.jpg") },
+      { title: "4", file: require("./santacruzbrewery/santacruzbrewery4.jpg") }
+    ]
+  },
+  islagrill: {
+    images: [
+      { title: "1", file: require("./islagrill/islagrill1.jpg") },
+      { title: "2", file: require("./islagrill/islagrill2.jpg") },
+      { title: "3", file: require("./islagrill/islagrill3.jpg") },
+      { title: "4", file: require("./islagrill/islagrill4.jpg") }
+    ]
+  },
+  lolo: {
+    images: [
+      { title: "1", file: require("./lolo/lolo1.jpg") },
+      { title: "2", file: require("./lolo/lolo2.jpg") },
+      { title: "3", file: require("./lolo/lolo3.jpg") },
+      { title: "4", file: require("./lolo/lolo4.jpg") }
+    ]
+  },
+  proinsular: {
+    images: [
+      { title: "1", file: require("./proinsular/proinsular1.jpg") },
+      { title: "2", file: require("./proinsular/proinsular2.png") },
+      { title: "3", file: require("./proinsular/proinsular3.jpg") },
+      { title: "4", file: require("./proinsular/proinsular4.jpg") }
+    ]
+  }
+};
+
+export default restaurantImages;
