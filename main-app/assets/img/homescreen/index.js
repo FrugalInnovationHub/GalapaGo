5a82a484fb9269d07c8cdaac7fefd3cfd4b86729
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-04-22 17:35:55 -0700

    refact homescreen file

---
+++ main-app/assets/img/homescreen/index.js
@@ -0,0 +1,11 @@
+const images = [
+  {
+    id: 1,
+    source: require("./Galapagos_homepage_whales.png")
+  },
+  { id: 2, source: require("./Galapagos_homepage_reptile.png") },
+  { id: 3, source: require("./Galapagos_homepage_tortoise.png") },
+  { id: 4, source: require("./Galapagos_homepage_coral.png") }
+];
+
+export default images;
--- main-app/screens/Home/HomeImageSwiper.js
+++ main-app/screens/Home/HomeImageSwiper.js
@@ -1,22 +1,10 @@
 import * as React from "react";
 import { Image, StyleSheet } from "react-native";
 import Swiper from "react-native-swiper";
-import Galapagos_homepage_whales from "../../app/assets/images/homescreen/Galapagos_homepage_whales.png";
-import Galapagos_homepage_coral from "../../app/assets/images/homescreen/Galapagos_homepage_coral.png";
-import Galapagos_homepage_reptile from "../../app/assets/images/homescreen/Galapagos_homepage_reptile.png";
-import Galapagos_homepage_tortoise from "../../app/assets/images/homescreen/Galapagos_homepage_tortoise.png";
-
-const images = [
-  {
-    id: 1,
-    source: Galapagos_homepage_whales
-  },
-  { id: 2, source: Galapagos_homepage_reptile },
-  { id: 3, source: Galapagos_homepage_tortoise },
-  { id: 4, source: Galapagos_homepage_coral }
-];
+import images from "../../assets/img/homescreen";
 
 const HomeImageSwiper = () => {
+  console.log(images);
   return (
     <Swiper>
       {images.map((item) => (
