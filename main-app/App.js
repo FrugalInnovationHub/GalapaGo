46cd24ba33bd001e2e1a949b1f11371991002607
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-05-07 17:03:45 -0700

    add database settings

--- main-app/App.js
+++ main-app/App.js
@@ -3,6 +3,29 @@ import NavigationApp from "./routers";
 import GlobalStateContext from "./context/globalContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getDatabase, ref, onValue } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkGmokW285RxesrlEOEGMOpL7DjBMvk_U",
+  authDomain: "galapago-d4744.firebaseapp.com",
+  databaseURL: "https://galapago-d4744-default-rtdb.firebaseio.com",
+  projectId: "galapago-d4744",
+  storageBucket: "galapago-d4744.appspot.com",
+  messagingSenderId: "508955483910",
+  appId: "1:508955483910:web:e910e43a67fdbca4c64887",
+  measurementId: "G-YFNZ5NMK3Q"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
 const Favorites = {
   restaurants: {},
   hotels: {},
@@ -32,10 +55,15 @@ export default class App extends React.Component {
 
   async init() {
     const data = await AsyncStorage.getItem("favorites");
-    console.log(JSON.parse(data));
     if (data) {
       this.updateFavorites(JSON.parse(data));
     }
+    // init database
+    // const starCountRef = ref(database);
+    // onValue(starCountRef, (snapshot) => {
+    //   const data = snapshot.val();
+    //   console.log("show database", data);
+    // });
   }
 
   componentDidMount() {
