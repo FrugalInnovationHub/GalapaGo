e84194ddb7e6344f7bab88de4be4b1fe74fa44e8
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-05-26 22:50:35 -0700

    add getImages function

---
+++ main-app/utils/getImages.js
@@ -0,0 +1,22 @@
+import { imageDir } from "../config/fileSystem";
+
+const getImages = (imageInfo, localImages) => {
+  const { url, images } = imageInfo;
+  if (!images || typeof images !== "object") {
+    return [];
+  }
+  const imageSource = images.map((item) => {
+    const source = localImages.find((_image) => item.id === _image.id);
+    if (source) {
+      return source;
+    } else {
+      return {
+        id: item.id,
+        file: `${imageDir}${url}${item.file}`
+      };
+    }
+  });
+
+  return imageSource;
+};
+export default getImages;
--- main-app/utils/index.js
+++ main-app/utils/index.js
@@ -4,11 +4,13 @@ import getDatabaseSnapshot from "./getDatabaseSnapshot";
 import getParsedDataFromAsyncStorage from "./getParsedDataFromAsyncStorage";
 import checkUpdateTime from "./checkUpdateTime";
 import updateImages from "./updateImages";
+import getImages from "./getImages";
 export {
   addFavorite,
   isEmpty,
   getDatabaseSnapshot,
   getParsedDataFromAsyncStorage,
   checkUpdateTime,
-  updateImages
+  updateImages,
+  getImages
 };
