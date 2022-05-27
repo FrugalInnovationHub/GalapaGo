7d85a36bedbc1d6dd286758a693d856384acd116
DUNKbaba <42214245+EmilioDeng98@users.noreply.github.com>
2022-05-26 21:47:48 -0700

    update images index

--- main-app/assets/img/transport/index.js
+++ main-app/assets/img/transport/index.js
@@ -1,163 +1,163 @@
 const travelAgenciesImages = {
   "mc-xploration": {
     images: [
-      { title: "1", file: require("./mc-xploration/1.jpg") },
-      { title: "2", file: require("./mc-xploration/2.jpg") },
-      { title: "3", file: require("./mc-xploration/3.jpg") },
-      { title: "4", file: require("./mc-xploration/4.jpg") }
+      { id: "1", file: require("./mc-xploration/1.jpg") },
+      { id: "2", file: require("./mc-xploration/2.jpg") },
+      { id: "3", file: require("./mc-xploration/3.jpg") },
+      { id: "4", file: require("./mc-xploration/4.jpg") }
     ]
   },
   "mn-celebrity-flora": {
     images: [
-      { title: "1", file: require("./mn-celebrity-flora/1.jpg") },
-      { title: "2", file: require("./mn-celebrity-flora/2.jpg") },
-      { title: "3", file: require("./mn-celebrity-flora/3.jpg") },
-      { title: "4", file: require("./mn-celebrity-flora/4.jpg") }
+      { id: "1", file: require("./mn-celebrity-flora/1.jpg") },
+      { id: "2", file: require("./mn-celebrity-flora/2.jpg") },
+      { id: "3", file: require("./mn-celebrity-flora/3.jpg") },
+      { id: "4", file: require("./mn-celebrity-flora/4.jpg") }
     ]
   },
   "mn-celebrity-xpedition": {
     images: [
-      { title: "1", file: require("./mn-celebrity-xpedition/1.jpg") },
-      { title: "2", file: require("./mn-celebrity-xpedition/2.jpg") },
-      { title: "3", file: require("./mn-celebrity-xpedition/3.jpg") },
-      { title: "4", file: require("./mn-celebrity-xpedition/4.jpg") }
+      { id: "1", file: require("./mn-celebrity-xpedition/1.jpg") },
+      { id: "2", file: require("./mn-celebrity-xpedition/2.jpg") },
+      { id: "3", file: require("./mn-celebrity-xpedition/3.jpg") },
+      { id: "4", file: require("./mn-celebrity-xpedition/4.jpg") }
     ]
   },
   "mn-endeavour": {
     images: [
-      { title: "1", file: require("./mn-endeavour/1.jpg") },
-      { title: "2", file: require("./mn-endeavour/2.jpg") },
-      { title: "3", file: require("./mn-endeavour/3.jpg") },
-      { title: "4", file: require("./mn-endeavour/4.jpg") }
+      { id: "1", file: require("./mn-endeavour/1.jpg") },
+      { id: "2", file: require("./mn-endeavour/2.jpg") },
+      { id: "3", file: require("./mn-endeavour/3.jpg") },
+      { id: "4", file: require("./mn-endeavour/4.jpg") }
     ]
   },
   "mn-isabela": {
     images: [
-      { title: "1", file: require("./mn-isabela/1.jpg") },
-      { title: "2", file: require("./mn-isabela/2.jpg") },
-      { title: "3", file: require("./mn-isabela/3.jpg") },
-      { title: "3", file: require("./mn-isabela/4.jpg") }
+      { id: "1", file: require("./mn-isabela/1.jpg") },
+      { id: "2", file: require("./mn-isabela/2.jpg") },
+      { id: "3", file: require("./mn-isabela/3.jpg") },
+      { id: "3", file: require("./mn-isabela/4.jpg") }
     ]
   },
   "mn-national-geographic": {
     images: [
-      { title: "1", file: require("./mn-national-geographic/1.jpg") },
-      { title: "2", file: require("./mn-national-geographic/2.jpg") },
-      { title: "3", file: require("./mn-national-geographic/3.jpg") }
+      { id: "1", file: require("./mn-national-geographic/1.jpg") },
+      { id: "2", file: require("./mn-national-geographic/2.jpg") },
+      { id: "3", file: require("./mn-national-geographic/3.jpg") }
     ]
   },
   "mv-galapagos-legend": {
     images: [
-      { title: "1", file: require("./mv-galapagos-legend/1.jpg") },
-      { title: "2", file: require("./mv-galapagos-legend/2.jpg") }
+      { id: "1", file: require("./mv-galapagos-legend/1.jpg") },
+      { id: "2", file: require("./mv-galapagos-legend/2.jpg") }
     ]
   },
   "y-albatros": {
     images: [
-      { title: "1", file: require("./y-albatros/1.jpg") },
-      { title: "2", file: require("./y-albatros/2.jpg") },
-      { title: "3", file: require("./y-albatros/3.jpg") },
-      { title: "4", file: require("./y-albatros/4.jpg") }
+      { id: "1", file: require("./y-albatros/1.jpg") },
+      { id: "2", file: require("./y-albatros/2.jpg") },
+      { id: "3", file: require("./y-albatros/3.jpg") },
+      { id: "4", file: require("./y-albatros/4.jpg") }
     ]
   },
   "y-anahi": {
     images: [
-      { title: "1", file: require("./y-anahi/1.jpg") },
-      { title: "2", file: require("./y-anahi/2.jpg") },
-      { title: "3", file: require("./y-anahi/3.jpg") },
-      { title: "4", file: require("./y-anahi/4.jpg") }
+      { id: "1", file: require("./y-anahi/1.jpg") },
+      { id: "2", file: require("./y-anahi/2.jpg") },
+      { id: "3", file: require("./y-anahi/3.jpg") },
+      { id: "4", file: require("./y-anahi/4.jpg") }
     ]
   },
   "y-beluga": {
     images: [
-      { title: "1", file: require("./y-beluga/1.jpg") },
-      { title: "2", file: require("./y-beluga/2.jpg") },
-      { title: "3", file: require("./y-beluga/3.jpg") },
-      { title: "4", file: require("./y-beluga/4.jpg") }
+      { id: "1", file: require("./y-beluga/1.jpg") },
+      { id: "2", file: require("./y-beluga/2.jpg") },
+      { id: "3", file: require("./y-beluga/3.jpg") },
+      { id: "4", file: require("./y-beluga/4.jpg") }
     ]
   },
   "y-cachalote": {
     images: [
-      { title: "1", file: require("./y-cachalote/1.jpg") },
-      { title: "2", file: require("./y-cachalote/2.jpg") },
-      { title: "3", file: require("./y-cachalote/3.jpg") },
-      { title: "4", file: require("./y-cachalote/4.jpg") }
+      { id: "1", file: require("./y-cachalote/1.jpg") },
+      { id: "2", file: require("./y-cachalote/2.jpg") },
+      { id: "3", file: require("./y-cachalote/3.jpg") },
+      { id: "4", file: require("./y-cachalote/4.jpg") }
     ]
   },
   "y-coral-i": {
     images: [
-      { title: "1", file: require("./y-coral-i/1.jpg") },
-      { title: "2", file: require("./y-coral-i/2.jpg") }
+      { id: "1", file: require("./y-coral-i/1.jpg") },
+      { id: "2", file: require("./y-coral-i/2.jpg") }
     ]
   },
   "y-coral-ii": {
     images: [
-      { title: "1", file: require("./y-coral-ii/1.jpg") },
-      { title: "2", file: require("./y-coral-ii/2.jpg") }
+      { id: "1", file: require("./y-coral-ii/1.jpg") },
+      { id: "2", file: require("./y-coral-ii/2.jpg") }
     ]
   },
   "y-cormorant": {
     images: [
-      { title: "1", file: require("./y-cormorant/1.jpg") },
-      { title: "2", file: require("./y-cormorant/2.jpg") },
-      { title: "3", file: require("./y-cormorant/3.jpg") },
-      { title: "4", file: require("./y-cormorant/4.jpg") }
+      { id: "1", file: require("./y-cormorant/1.jpg") },
+      { id: "2", file: require("./y-cormorant/2.jpg") },
+      { id: "3", file: require("./y-cormorant/3.jpg") },
+      { id: "4", file: require("./y-cormorant/4.jpg") }
     ]
   },
   "y-daphne": {
     images: [
-      { title: "1", file: require("./y-daphne/1.jpg") },
-      { title: "2", file: require("./y-daphne/2.jpg") },
-      { title: "3", file: require("./y-daphne/3.jpg") },
-      { title: "4", file: require("./y-daphne/4.jpg") }
+      { id: "1", file: require("./y-daphne/1.jpg") },
+      { id: "2", file: require("./y-daphne/2.jpg") },
+      { id: "3", file: require("./y-daphne/3.jpg") },
+      { id: "4", file: require("./y-daphne/4.jpg") }
     ]
   },
   "y-infinity": {
     images: [
-      { title: "1", file: require("./y-infinity/1.jpg") },
-      { title: "2", file: require("./y-infinity/2.jpg") },
-      { title: "3", file: require("./y-infinity/3.jpg") },
-      { title: "4", file: require("./y-infinity/4.jpg") }
+      { id: "1", file: require("./y-infinity/1.jpg") },
+      { id: "2", file: require("./y-infinity/2.jpg") },
+      { id: "3", file: require("./y-infinity/3.jpg") },
+      { id: "4", file: require("./y-infinity/4.jpg") }
     ]
   },
   "y-jesus-del-gran-poder": {
     images: [
-      { title: "1", file: require("./y-jesus-del-gran-poder/1.jpg") },
-      { title: "2", file: require("./y-jesus-del-gran-poder/2.jpg") },
-      { title: "3", file: require("./y-jesus-del-gran-poder/3.jpg") },
-      { title: "4", file: require("./y-jesus-del-gran-poder/4.jpg") }
+      { id: "1", file: require("./y-jesus-del-gran-poder/1.jpg") },
+      { id: "2", file: require("./y-jesus-del-gran-poder/2.jpg") },
+      { id: "3", file: require("./y-jesus-del-gran-poder/3.jpg") },
+      { id: "4", file: require("./y-jesus-del-gran-poder/4.jpg") }
     ]
   },
   "y-nemo-ii": {
     images: [
-      { title: "1", file: require("./y-nemo-ii/1.jpg") },
-      { title: "2", file: require("./y-nemo-ii/2.jpg") },
-      { title: "3", file: require("./y-nemo-ii/3.jpg") },
-      { title: "4", file: require("./y-nemo-ii/4.jpg") }
+      { id: "1", file: require("./y-nemo-ii/1.jpg") },
+      { id: "2", file: require("./y-nemo-ii/2.jpg") },
+      { id: "3", file: require("./y-nemo-ii/3.jpg") },
+      { id: "4", file: require("./y-nemo-ii/4.jpg") }
     ]
   },
   "y-reina-silvia": {
     images: [
-      { title: "1", file: require("./y-reina-silvia/1.jpg") },
-      { title: "2", file: require("./y-reina-silvia/2.jpg") },
-      { title: "3", file: require("./y-reina-silvia/3.jpg") },
-      { title: "4", file: require("./y-reina-silvia/4.jpg") }
+      { id: "1", file: require("./y-reina-silvia/1.jpg") },
+      { id: "2", file: require("./y-reina-silvia/2.jpg") },
+      { id: "3", file: require("./y-reina-silvia/3.jpg") },
+      { id: "4", file: require("./y-reina-silvia/4.jpg") }
     ]
   },
   "y-san-jacinto": {
     images: [
-      { title: "1", file: require("./y-san-jacinto/1.jpg") },
-      { title: "2", file: require("./y-san-jacinto/2.jpg") },
-      { title: "3", file: require("./y-san-jacinto/3.jpg") },
-      { title: "4", file: require("./y-san-jacinto/4.jpg") }
+      { id: "1", file: require("./y-san-jacinto/1.jpg") },
+      { id: "2", file: require("./y-san-jacinto/2.jpg") },
+      { id: "3", file: require("./y-san-jacinto/3.jpg") },
+      { id: "4", file: require("./y-san-jacinto/4.jpg") }
     ]
   },
   "yate-santa-fe": {
     images: [
-      { title: "1", file: require("./yate-santa-fe/1.jpg") },
-      { title: "2", file: require("./yate-santa-fe/2.jpg") },
-      { title: "3", file: require("./yate-santa-fe/3.jpg") },
-      { title: "4", file: require("./yate-santa-fe/4.jpg") }
+      { id: "1", file: require("./yate-santa-fe/1.jpg") },
+      { id: "2", file: require("./yate-santa-fe/2.jpg") },
+      { id: "3", file: require("./yate-santa-fe/3.jpg") },
+      { id: "4", file: require("./yate-santa-fe/4.jpg") }
     ]
   }
 };
